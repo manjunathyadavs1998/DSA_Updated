ff66965204f8f25e7086c7b0592061bd390c3945
MSA63 <manjunathyadavs1998@gmail.com>
2022-11-01 13:54:00 +0530

    DSA updated

--- src/Arrays/arrays.java
+++ src/Arrays/arrays.java
@@ -1,9 +1,6 @@
 package Arrays;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class arrays {
 
@@ -292,6 +289,25 @@ public class arrays {
             }
         }
         return -1;
+    }
+
+    public static List<Integer> findFirstNegativeOfSizeKSubarray(int[] arr, int n, int k)
+    {
+        ArrayList<Integer>arrayList=new ArrayList<>();
+        boolean flag=true;
+        for(int i=0; i<k; i++)
+        {
+            if(arr[i]<0)
+            {
+                arrayList.add(arr[i]);
+                flag=false;
+                break;
+            }
+        }
+        if(flag==true)
+            arrayList.add(0);
+
+        return arrayList;
     }
 
 
