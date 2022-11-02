21a01815f33f20af488e7a03b3a5e25bddc3e99d
Manjunath <manjunathyadavs1998@gmail.com>
2022-11-02 09:10:49 +0530

    Update DSA Codes

--- src/LinkedList/linkedlist.java
+++ src/LinkedList/linkedlist.java
@@ -6,25 +6,13 @@ public class linkedlist {
     static Node head=null;
 
     public static void addElementAtEnd(int data) {   //Logic to add Element at end of the Linked list;
-//        Node newNode=new Node(data);
-//        if(head==null)
-//            head=newNode;
-//        else {
-//            Node temp=head;
-//            while(temp.next!=null) temp=temp.next;
-//            temp.next=newNode;
-//        }
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-        } else {
+        Node newNode=new Node(data);
+        if(head==null)
+            head=newNode;
+        else {
             Node temp = head;
-
-            while (temp.next != null) {
-                temp = temp.next;
-            }
+            while (temp.next != null) temp = temp.next;
             temp.next = newNode;
-
         }
     }
 
